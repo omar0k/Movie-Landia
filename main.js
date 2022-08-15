9eb58495e0886b72c8ecc1697433b0bafc4e466a
omark01 <omark012000@gmail.com>
2022-08-15 05:43:34 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,147 @@
+//Create 2 more buttons that appear once the search function is called/when user searches for a movie
+//
+
+import API_KEY from "./key.js";
+let baseUrl = "https://api.themoviedb.org/3/";
+let movieList = document.getElementById("movie-list");
+let api_URL = baseUrl + "/trending/all/day?api_key=" + API_KEY;
+let currentPage = 1;
+let buttonsDiv = document.getElementById("buttons-div");
+let previousPage = document.getElementById("previous-page");
+let nextPage = document.getElementById("next-page");
+let previousPageSearch = document.getElementById("previous-page-search");
+let nextPageSearch = document.getElementById("next-page-search");
+
+nextPageSearch.style.display = "none";
+previousPageSearch.style.display = "none";
+
+function getMovies(url, currentPage) {
+  //Can add "&page=1" to the end of the url to get the first page of results
+  url = url + "&page=" + currentPage;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      showMovies(data.results);
+    });
+}
+document.getElementById("home-btn").addEventListener("click", () => {
+  nextPageSearch.style.display = "none";
+  previousPageSearch.style.display = "none";
+  nextPage.style.display = "inline";
+  previousPage.style.display = "inline";
+
+  getMovies(api_URL, currentPage);
+  console.log("clicked");
+});
+nextPage.addEventListener("click", () => {
+  currentPage++;
+  console.log(currentPage);
+  getMovies(api_URL, currentPage);
+});
+
+previousPage.addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    getMovies(api_URL, currentPage);
+  } else {
+    return;
+  }
+});
+nextPageSearch.addEventListener("click", () => {
+  let search = document.getElementById("search-bar").value;
+  currentPage++;
+  let search_url =
+    baseUrl +
+    `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${search}`;
+  console.log("search url", search_url);
+  getMovies(search_url, currentPage);
+});
+previousPageSearch.addEventListener("click", () => {
+  let search = document.getElementById("search-bar").value;
+  let search_url =
+    baseUrl +
+    `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${search}`;
+  console.log("search url", search_url);
+  if (currentPage > 1) {
+    currentPage--;
+    getMovies(search_url, currentPage);
+  } else {
+    return;
+  }
+});
+function showMovies(data) {
+  movieList.innerHTML = "";
+  if (data.length == 0) {
+    movieList.innerHTML = "<h1>No Movies Found</h1>";
+  }
+  data.forEach((movie) => {
+    const { id, title, name, vote_average, poster_path, overview } = movie;
+    if (poster_path === null) {
+      console.log(name || title, id);
+      return;
+    }
+    movieList.innerHTML += `
+    <div movie-info>
+        <h3 class='movie-title'>${name || title}</h3>
+        <h4>${id}</h4>
+        <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="No Poster" onerror="this.src='/backupimage.jpg'"/>
+        <p>${overview}</p>
+        <span class="${getColor(vote_average)}">${vote_average}</span>
+    </div>
+    `;
+  });
+}
+
+//when the search button is clicked
+document.getElementById("search-btn").addEventListener("click", () => {
+  currentPage = 1;
+  document.query;
+  let search = document.getElementById("search-bar").value;
+  if (search == "") {
+    return;
+  } else {
+    nextPage.style.display = "none";
+    previousPage.style.display = "none";
+    nextPageSearch.style.display = "inline";
+    previousPageSearch.style.display = "inline";
+    let search_url =
+      baseUrl +
+      `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${search}`;
+    console.log("search url", search_url);
+    getMovies(search_url, currentPage);
+  }
+});
+
+document
+  .getElementById("search-bar")
+  .addEventListener("keyup", function (event) {
+    currentPage = 1;
+    const searchTerm = event.target.value;
+    console.log(searchTerm);
+    if (
+      event.keyCode === 13 &&
+      document.getElementById("search-bar").value != ""
+    ) {
+      nextPage.style.display = "none";
+      previousPage.style.display = "none";
+      nextPageSearch.style.display = "inline";
+      previousPageSearch.style.display = "inline";
+      event.preventDefault();
+      getMovies(
+        baseUrl +
+          `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${searchTerm}`
+      );
+    }
+  });
+
+function getColor(vote) {
+  if (vote >= 7) {
+    return "green";
+  } else if (vote >= 4) {
+    return "yellow";
+  } else {
+    return "red";
+  }
+}
+
+getMovies(api_URL, currentPage);
